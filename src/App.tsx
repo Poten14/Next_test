c745890acd4c8f12182b61660578d2a50a9f09df
alloen9425 <allone9425@gmail.com>
2024-08-12 13:18:39 +0900

    [#25]feat : test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world! wow!!!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">Hello world! wow!!!hi</h1>
+  );
 }
